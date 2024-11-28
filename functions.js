a89c61679fb57d55e807f3bdc602f0caa45b487c
mpcooper23 <mpcooper23@gmail.com>
2024-11-28 15:54:28 -0600

    e

--- functions.js
+++ functions.js
@@ -37,14 +37,33 @@ animals[i] = replacement
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-
+function remove(animals, name){
+for(let i = 0; i < animals.length; i++){
+    if(animal[i] === name){
+delete name
+    }
+}
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function add(animals, animal){
+for(let i = 0; i < animals.length; i++){
+    let animalKeys = animals[i]
+    for(let name in animals[i]){
+    if(animalKeys[animal.name].length > 0){// Checks that the animal Object has a `name` property with a length > 0.
+if(animalKeys[animal.species].length > 0){//Checks that the animal Object has a `species` property with a length > 0.
+    if(animalKeys[animal.name] !== animalKeys[animal.name]){// Has a **unique** name, meaning no other animals have that name.
+animals.push(animal)// Adds this new Object to the `animals` Array, **only** if all the other conditions pass.
+    }
+}
+    }
+}
+}
+}
 
 /**
  * You did it! You're all done with Matchy!
