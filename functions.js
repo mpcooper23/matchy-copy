9115bba8f11cb994399c684f7c1f6b3cea5de3c5
mpcooper23 <mpcooper23@gmail.com>
2024-12-11 09:51:18 -0600

    2

--- functions.js
+++ functions.js
@@ -34,6 +34,15 @@ return animals[i]
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+/*## Step 2 - Replace
+ 1. Write a function declaration called `replace` with a signature of `replace(animals, name, replacement) { //... } ` that:
+   - Takes 3 parameters, an Array of animals, a String representing  the name of an animal on which to perform a search, and an Object that represents the replacement animal.
+   - If an animal with that name exists within the `animals` Array, replace it's entire Object with the replacement Object.
+   - Otherwise do nothing.
+ 2. Preview the `index.html` page to test it on the website.
+
+*/
+
 function replace (animals, name, replacement){
 for(let i = 0; i < animals.length; i++){
     if(animals[i] === name){
